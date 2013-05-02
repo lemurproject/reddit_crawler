3d0686139be7791b41f7395cae003b86aeb9fdf8
Shriphani Palakodety <shriphanip@gmail.com>
2013-05-01 20:07:32 -0400

    ironed out log bugs

--- src/reddit_crawler/read_posts.clj
+++ src/reddit_crawler/read_posts.clj
@@ -5,7 +5,10 @@
             [clojure.tools.cli :as cli]
             [clojure.walk :as walk]
             [clj-time.coerce :as clj-time-coerce]
-            [clj-time.core :as clj-time]))
+            [clj-time.core :as clj-time]
+            [clojure.string :as string]))
+
+(def reddit-stats-file "/bos/www/htdocs/spalakod/clueweb12pp/reddit-status.txt")
 
 ; Open and read the reddit crawler files
 (defn read-posts
@@ -34,9 +37,12 @@
 (defn -main
   [& args]
   (let [[dict [posts-file] banner] (cli/cli args)
-        [num-dates last-post-date] (last-date posts-file)]
-      (spit "/bos/www/htdocs/spalakod/clueweb12pp/reddit-status.txt" 
-            (format "Report Time: %s\tNumber of Posts: %d\tMost Recent Date Downloaded: %s" 
-                    (.toString clj-time/now) 
-                    num-dates 
-                    (.toString last-post-date)))))
+        [num-dates last-post-date] (last-date posts-file)
+        stats-lines (string/split-lines (slurp reddit-stats-file))]
+      
+          (spit reddit-stats-file 
+                (string/join "\n" (cons (format "Report Time: %s\tNumber of Posts: %d\tMost Recent Date Downloaded: %s" 
+                                          (.toString (clj-time/now))
+                                          num-dates 
+                                          (.toString last-post-date))
+                                  stats-lines)))))
